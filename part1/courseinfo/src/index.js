4b50fbd21c5142808b60d35031696bba71df63f2
alan-cash <contactalanvalencia@gmail.com>
2022-11-13 16:58:59 +0100

    perf(part1/courseinfo): Make exercise 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,30 +3,42 @@ import ReactDOM from 'react-dom/client'
 
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Content = ({ part, exercises }) => (
-  <p>
-    {part} {exercises}
-  </p>
-)
+const Content = ({ part }) => {
+  const { name, exercises } = part
+
+console.log({part})
+
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
 
 const Total = ({ amount }) => <p>Number of exercises {amount}</p>
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total amount={exercises1 + exercises2 + exercises3} />
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <Total amount={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
