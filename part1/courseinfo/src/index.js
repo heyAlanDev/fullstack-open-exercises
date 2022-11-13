f91beddbe0716cb814647beaf3c95a17e9a0f43c
alan-cash <contactalanvalencia@gmail.com>
2022-11-13 17:38:16 +0100

    feat(part1/courseinfo): Make the exercise 1.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,42 +3,50 @@ import ReactDOM from 'react-dom/client'
 
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Content = ({ part }) => {
-  const { name, exercises } = part
-
-console.log({part})
-
+const Content = ({ parts }) => {
   return (
-    <p>
-      {name} {exercises}
-    </p>
+    <div>
+      {parts.map(({ name, exercises }, index) => (
+        <p key={index}>
+          {name} {exercises}
+        </p>
+      ))}
+    </div>
   )
 }
 
-const Total = ({ amount }) => <p>Number of exercises {amount}</p>
+const Total = ({ parts = [] }) => {
+  const totalExercises = parts
+    .map(({ exercises }) => exercises)
+    .reduce((prev, current) => {
+      return prev + current
+    })
+
+  return <p>Total number exercises {totalExercises}</p>
+}
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} />
-      <Content part={part2} />
-      <Content part={part3} />
-      <Total amount={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
